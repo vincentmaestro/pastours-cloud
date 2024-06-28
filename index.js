90e0fa240c87814556cd67ea811efdbcad8c2fe7
vincentmaestro <maestroobika@gmail.com>
2024-06-28 19:22:23 +0100

    major fixes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const path = require("path");
-process.env['NODE_CONFIG_DIR'] = path.join(path.resolve("./"),"config/");
+process.env["NODE_CONFIG_DIR"] = __dirname + "/config/";
 const express = require('express');
 const config = require('config');
 const logger = require('./start/logger');
