094475563b253e85897727e1035d067198e0b972
vincentmaestro <maestroobika@gmail.com>
2024-06-29 04:46:40 +0100

    major fixes

--- start/logger.js
+++ start/logger.js
@@ -1,15 +1,14 @@
-require('express-async-errors');
-require('winston-mongodb');
 const config = require('config');
 const { createLogger, transports, format } = require('winston');
 const { prettyPrint, simple } = format;
-const connectionString = config.get('dbConnString');
+require('express-async-errors');
+require('winston-mongodb');
 
 const logger = createLogger({
     transports: [
         new transports.Console({ handleExceptions: true, handleRejections: true, format: prettyPrint() }),
         new transports.File({ handleExceptions: true, handleRejections: true, filename: 'logs.log', level: 'error', format: simple() }),
-        new transports.MongoDB({ handleExceptions: true, handleRejections: true, db: connectionString, level: 'error', format: simple() })
+        new transports.MongoDB({ handleExceptions: true, handleRejections: true, db: config.get('dbConnString'), level: 'error', format: simple() })
     ]
 });
 
